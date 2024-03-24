11f65f4ced9ccbe756f5a0f8b216248f87202b10
Newtanmukh <importantnewton@gmail.com>
2024-03-24 17:58:26 +0530

    App and index updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 
 import Card from "./Card";
-
+import Button from "./Button";
 
 function App() {
   return(
     <>
+        {/* <Card></Card>
         <Card></Card>
         <Card></Card>
         <Card></Card>
         <Card></Card>
         <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card></Card> */
+        
+      <Button></Button>
+        
+        }
     </>
             
   );
