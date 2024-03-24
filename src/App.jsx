1c7ec32bcd9f95f771d8c287434de4926ce8cc94
Newtanmukh <importantnewton@gmail.com>
2024-03-24 13:55:18 +0530

    commit done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Footer from "./Footer";
+import Food from "./Food";
+
+
+function App() {
+  return(
+    <>
+        <Header/>
+        <Footer/>
+        <Food/>
+    </>
+            
+  );
+}
+
+export default App
