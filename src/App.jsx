6ab269f7bf9f45a5e945089011cd2f0d88b843ce
Newtanmukh <importantnewton@gmail.com>
2024-03-25 20:39:17 +0530

    MyComponent and App,Food and Footer updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import Header from "./Header"
-import Footer from "./Footer";
-import Food from "./Food";
+import MyComponent from "./MyComponent";
 
 
 function App() {
   return(
     <>
-        <Header/>
-        <Footer/>
-        <Food/>
+      <MyComponent></MyComponent>
     </>
             
   );
